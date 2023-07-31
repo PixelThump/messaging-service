f80fa865da8cf4661d2572bdafd655a9883f51ac
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-31 13:51:55 +0200

    Add files via upload

--- src/main/java/com/pixelthump/messagingservice/service/SeshServiceImpl.java
+++ src/main/java/com/pixelthump/messagingservice/service/SeshServiceImpl.java
@@ -1,6 +1,13 @@
 package com.pixelthump.messagingservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.pixelthump.messagingservice.service.model.SeshInfo;
 import com.pixelthump.messagingservice.service.model.SeshState;
@@ -9,18 +16,30 @@ import com.pixelthump.messagingservice.stomp.model.message.CommandStompMessage;
 @Component
 public class SeshServiceImpl implements SeshService{
 
-	@Override
-	public SeshInfo getSeshInfo(String seshCode) {
-		return null;
+	private final RestTemplate restTemplate;
+	@Value("${pixelthump.backend-basepath}")
+	private String backendBasePath;
+
+	@Autowired
+	public SeshServiceImpl(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
 	}
 
-	@Override
-	public SeshInfo hostSesh(String seshCode) {
-		return null;
+	private SeshInfo getSeshInfo(String seshCode) {
+
+		try {
+			String apiUrl = backendBasePath + "/seshservice/seshs/" + seshCode;
+			ResponseEntity<SeshInfo> responseEntity = restTemplate.getForEntity(apiUrl,SeshInfo.class);
+			return responseEntity.getBody();
+		}catch (RestClientException e){
+			throw new ResponseStatusException(HttpStatusCode.valueOf(404), e.getMessage());
+		}
 	}
 
 	@Override
 	public SeshState joinAsController(String seshCode, String playerName, String socketId) {
+
+		SeshInfo seshInfo = getSeshInfo(seshCode);
 		return null;
 	}
 
@@ -31,6 +50,7 @@ public class SeshServiceImpl implements SeshService{
 
 	@Override
 	public void sendCommandToSesh(CommandStompMessage message, String seshCode) {
+
 
 	}
 }
