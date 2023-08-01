b8413983cec904401dafe2e4ffe28f7cc5a2648b
roboter5123 <pascal.roboter@hotmail.com>
2023-08-01 08:58:45 +0200

    Some code cleanup

---
+++ src/main/java/com/pixelthump/messagingservice/rest/model/MessagingSeshStateWrapper.java
@@ -0,0 +1,8 @@
+package com.pixelthump.messagingservice.rest.model;
+import lombok.Data;
+
+@Data
+public class MessagingSeshStateWrapper {
+
+    private Object state;
+}
--- src/main/java/com/pixelthump/messagingservice/rest/model/MessagingSeshUpdate.java
+++ src/main/java/com/pixelthump/messagingservice/rest/model/MessagingSeshUpdate.java
@@ -1,10 +1,9 @@
 package com.pixelthump.messagingservice.rest.model;
-import com.pixelthump.messagingservice.service.model.SeshStateWrapper;
 import lombok.Data;
 
 @Data
 public class MessagingSeshUpdate {
 
-    private SeshStateWrapper host;
-    private SeshStateWrapper controller;
+    private MessagingSeshStateWrapper host;
+    private MessagingSeshStateWrapper controller;
 }
