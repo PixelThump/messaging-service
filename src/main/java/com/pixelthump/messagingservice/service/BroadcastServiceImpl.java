a9d968034cb155f032a06360d4a8c7bca2e8abef
roboter5123 <pascal.roboter@hotmail.com>
2023-08-19 09:46:11 +0200

    Payloads are now wrapped ina generic message

--- src/main/java/com/pixelthump/messagingservice/service/BroadcastServiceImpl.java
+++ src/main/java/com/pixelthump/messagingservice/service/BroadcastServiceImpl.java
@@ -2,6 +2,7 @@ package com.pixelthump.messagingservice.service;
 import com.pixelthump.messagingservice.repository.PlayerRepository;
 import com.pixelthump.messagingservice.repository.model.Player;
 import com.pixelthump.messagingservice.repository.model.Role;
+import com.pixelthump.messagingservice.service.model.message.StompMessage;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -16,12 +17,15 @@ public class BroadcastServiceImpl implements BroadcastService {
 
     private final SimpMessagingTemplate messagingTemplate;
     private static final String SESH_BASE_PATH = "/topic/sesh/";
+
+    private final StompMessageFactory stompMessageFactory;
     private final PlayerRepository playerRepository;
 
     @Autowired
-    public BroadcastServiceImpl(SimpMessagingTemplate messagingTemplate, PlayerRepository playerRepository) {
+    public BroadcastServiceImpl(SimpMessagingTemplate messagingTemplate, StompMessageFactory stompMessageFactory, PlayerRepository playerRepository) {
 
         this.messagingTemplate = messagingTemplate;
+        this.stompMessageFactory = stompMessageFactory;
         this.playerRepository = playerRepository;
     }
 
@@ -63,6 +67,7 @@ public class BroadcastServiceImpl implements BroadcastService {
     private void broadcastToPlayer(Player player, Object payload) {
 
         String destination = SESH_BASE_PATH + player.getPlayerId().getSeshCode() + "/" + player.getPlayerId().getPlayerName();
-        messagingTemplate.convertAndSend(destination, payload);
+        StompMessage message = stompMessageFactory.getGenericMessage(payload);
+        messagingTemplate.convertAndSend(destination, message);
     }
 }
--- src/main/java/com/pixelthump/messagingservice/service/model/message/GenericStompMessage.java
+++ src/main/java/com/pixelthump/messagingservice/service/model/message/GenericStompMessage.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GenericStompMessage implements StompMessage {
 
-    private Object object;
+    private Object payload;
 }
