a79ce3646366d00a745644f00dce50c32fd916f1
roboter5123 <pascal.roboter@hotmail.com>
2023-08-19 10:00:55 +0200

    Fixed tests

--- src/test/java/com/pixelthump/messagingservice/service/BroadcastServiceImplTest.java
+++ src/test/java/com/pixelthump/messagingservice/service/BroadcastServiceImplTest.java
@@ -4,6 +4,7 @@ import com.pixelthump.messagingservice.repository.PlayerRepository;
 import com.pixelthump.messagingservice.repository.model.Player;
 import com.pixelthump.messagingservice.repository.model.PlayerId;
 import com.pixelthump.messagingservice.repository.model.Role;
+import com.pixelthump.messagingservice.service.model.message.GenericStompMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,18 +38,19 @@ class BroadcastServiceImplTest {
 
         List<Player> players = getPlayers();
         when(playerRepository.findByPlayerId_SeshCode(any())).thenReturn(players);
+        String payload = "payload";
+        when(factory.getGenericMessage(any())).thenReturn(new GenericStompMessage(payload));
 
         List<String> recipients = new ArrayList<>();
         recipients.add(players.get(0).getPlayerId().getPlayerName());
         recipients.add(players.get(1).getPlayerId().getPlayerName());
         recipients.add(players.get(2).getPlayerId().getPlayerName());
 
-        String payload = "payload";
         broadcastService.broadcastToListOfPlayers(seshCode, recipients, payload);
 
-        verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + players.get(0).getPlayerId().getSeshCode() + "/" + players.get(0).getPlayerId().getPlayerName(), payload);
-        verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + players.get(1).getPlayerId().getSeshCode() + "/" + players.get(1).getPlayerId().getPlayerName(), payload);
-        verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + players.get(2).getPlayerId().getSeshCode() + "/" + players.get(2).getPlayerId().getPlayerName(), payload);
+        verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + players.get(0).getPlayerId().getSeshCode() + "/" + players.get(0).getPlayerId().getPlayerName(), new GenericStompMessage(payload));
+        verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + players.get(1).getPlayerId().getSeshCode() + "/" + players.get(1).getPlayerId().getPlayerName(), new GenericStompMessage(payload));
+        verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + players.get(2).getPlayerId().getSeshCode() + "/" + players.get(2).getPlayerId().getPlayerName(), new GenericStompMessage(payload));
     }
 
     @Test
@@ -60,14 +62,16 @@ class BroadcastServiceImplTest {
         Map<String, Object> recipients = new HashMap<>();
         recipients.put("abcd", "efgh");
         recipients.put("1234", 5678);
+        when(factory.getGenericMessage("efgh")).thenReturn(new GenericStompMessage("efgh"));
+        when(factory.getGenericMessage(5678)).thenReturn(new GenericStompMessage(5678));
         broadcastService.broadcastToDifferentPlayers(seshCode, recipients);
 
         for (String recipientName : recipients.keySet()) {
 
-            verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + seshCode + "/" + recipientName, recipients.get(recipientName));
+            verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + seshCode + "/" + recipientName, new GenericStompMessage(recipients.get(recipientName)));
         }
 
-        verify(messagingTemplate, never()).convertAndSend(SESH_BASE_PATH + seshCode + "/" + players.get(2).getPlayerId().getPlayerName(), recipients.get(players.get(2).getPlayerId().getPlayerName()));
+        verify(messagingTemplate, never()).convertAndSend(SESH_BASE_PATH + seshCode + "/" + players.get(2).getPlayerId().getPlayerName(), new GenericStompMessage(recipients.get(players.get(2).getPlayerId().getPlayerName())));
     }
 
     private List<Player> getPlayers() {
@@ -90,9 +94,10 @@ class BroadcastServiceImplTest {
         Player player = players.get(0);
         String playerName = player.getPlayerId().getPlayerName();
         when(playerRepository.findByPlayerId_SeshCodeAndPlayerId_PlayerName(seshCode, playerName)).thenReturn(player);
+        when(factory.getGenericMessage("payload")).thenReturn(new GenericStompMessage("payload"));
 
         broadcastService.broadcastToSinglePlayer(seshCode, playerName, "payload");
-        verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + seshCode + "/" + playerName, "payload");
+        verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + seshCode + "/" + playerName, new GenericStompMessage("payload"));
     }
 
     @Test
@@ -100,12 +105,13 @@ class BroadcastServiceImplTest {
 
         List<Player> players = getPlayers();
         when(playerRepository.findByPlayerId_SeshCode(seshCode)).thenReturn(players);
+        when(factory.getGenericMessage("payload")).thenReturn(new GenericStompMessage("payload"));
 
         broadcastService.broadcastToAllPlayers(seshCode, "payload");
 
         for (Player player : players) {
 
-            verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + seshCode + "/" + player.getPlayerId().getPlayerName(), "payload");
+            verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + seshCode + "/" + player.getPlayerId().getPlayerName(), new GenericStompMessage("payload"));
         }
     }
 
@@ -114,12 +120,13 @@ class BroadcastServiceImplTest {
 
         List<Player> players = getPlayers();
         when(playerRepository.findByPlayerId_SeshCodeAndRole(seshCode, Role.CONTROLLER)).thenReturn(players);
+        when(factory.getGenericMessage("payload")).thenReturn(new GenericStompMessage("payload"));
 
         broadcastService.broadcastToRole(seshCode, Role.CONTROLLER, "payload");
 
         for (Player player : players) {
 
-            verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + seshCode + "/" + player.getPlayerId().getPlayerName(), "payload");
+            verify(messagingTemplate).convertAndSend(SESH_BASE_PATH + seshCode + "/" + player.getPlayerId().getPlayerName(), new GenericStompMessage("payload"));
         }
     }
 }
